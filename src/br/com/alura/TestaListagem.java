a9d59a48e04f576090892567b30734350cb67265
João Gabriel Nascimento da Silva <jgabriel10x@gmail.com>
2021-08-10 20:20:28 -0300

    aula 2, video 1, Listagem com Statement

---
+++ src/br/com/alura/TestaListagem.java
@@ -0,0 +1,35 @@
+package br.com.alura;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TestaListagem {
+
+	public static void main(String[] args) throws SQLException {
+		
+		Connection connection = DriverManager.getConnection
+				("jdbc:mysql://localhost:1604/loja_virtual?useTimezone=true&serverTimezone=UTC", "root", "j18g180604");
+
+		Statement stm = connection.createStatement();
+		stm.execute("SELECT ID, NOME, DESCRICAO FROM PRODUTO");
+		
+		ResultSet rst = stm.getResultSet();
+		
+		while(rst.next()) {
+			Integer id = rst.getInt("ID");
+			System.out.println(id);
+			String nome = rst.getString("NOME");
+			System.out.println(nome);
+			String descricao = rst.getString("DESCRICAO");
+			System.out.println(descricao);
+		}
+		
+		connection.close();
+
+	}
+
+}
+
