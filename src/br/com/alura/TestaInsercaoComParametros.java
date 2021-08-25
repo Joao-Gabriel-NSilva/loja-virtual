05caae44e0d1819c8349fdcb769a46ed7f48be9a
João Gabriel Nascimento da Silva <jgabriel10x@gmail.com>
2021-08-25 17:15:32 -0300

    aula 4, video 1, assumindo o controle

--- src/br/com/alura/TestaInsercaoComParametros.java
+++ src/br/com/alura/TestaInsercaoComParametros.java
@@ -9,17 +9,25 @@ import java.sql.Statement;
 public class TestaInsercaoComParametros {
 
 	public static void main(String[] args) throws SQLException {
-
-		String nome = "Mouse'";
-		String descricao = "Mouse sem fio); delete from Produtos;";
 		
 		Connection connection = new ConnectionFactory().recuperarConexao();
+		connection.setAutoCommit(false);
 
 		PreparedStatement stm = connection.prepareStatement(
 				"INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)", Statement.RETURN_GENERATED_KEYS);
 		
+		adicionarVariavel("SmartTV", "45 polegadas", stm);
+		adicionarVariavel("Radio", "Radio de bateria", stm);
+	}
+
+	private static void adicionarVariavel(String nome, String descricao, PreparedStatement stm) throws SQLException {
 		stm.setString(1, nome);
 		stm.setString(2, descricao);
+		
+		/*if(nome.equals("Radio")) {
+			throw new RuntimeException("Não foi possivel adicionar o produto");
+		}*/
+		
 		stm.execute();
 
 		ResultSet rst = stm.getGeneratedKeys();
@@ -29,7 +37,7 @@ public class TestaInsercaoComParametros {
 			System.out.println("O id criado foi: " + id);
 		}
 
-		connection.close();
+		rst.close();
 	}
 
 }
